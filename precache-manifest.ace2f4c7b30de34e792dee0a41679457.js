2a80efa07f053e2049b3f9347b6f1dd9d4c536ee
Rufaidashafiq <rufaidashafik@gmail.com>
2020-11-15 01:12:41 +0500

    Updates

---
+++ precache-manifest.ace2f4c7b30de34e792dee0a41679457.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ff7d7f607dbff208c3c1ebe315771c",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "f887a010af8c233b0686",
+    "url": "/react-deploy/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "da576d7c7e4b0f6ff0a9",
+    "url": "/react-deploy/static/js/2.116ab8a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-deploy/static/js/2.116ab8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f887a010af8c233b0686",
+    "url": "/react-deploy/static/js/main.20725ab4.chunk.js"
+  },
+  {
+    "revision": "87f6f8a9b4457b6460f1",
+    "url": "/react-deploy/static/js/runtime-main.5028925c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27b23a52df0c85503b9fe1b36595cb5a",
+    "url": "/react-deploy/static/media/vs.27b23a52.png"
+  }
+]);
